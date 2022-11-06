080942730c6081b163793ab21ef625b2456005d2
selehadin-cyber <sele4416@gmail.com>
2022-11-06 11:30:48 +0300

    switch homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     extend: {
       screens: {
         xs: { max: "852px" },
+        "1072": "1072px"
       },
+      backgroundImage: {
+        'gradient-bg':
+          "linear-gradient(170deg, rgb(17 24 39) 0%, hsl(0, 0%, 6%) 30%)",
+      }
     },
   },
   plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
