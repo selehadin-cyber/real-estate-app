bbe1f6132c45d29f2038e3ced24c069e15bd0186
selehadin-cyber <sele4416@gmail.com>
2022-12-02 08:48:27 +0300

    Fix bug

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -42,7 +42,7 @@ const Slider: React.FC<HomesArray> = ({ homesArray }) => {
     >
       <div className="swiper-wrapper">
         {homesArray.map((home) => (
-          <SwiperSlide className="swiper-slide w-[290px] 1072:w-[320px]   ">
+          <SwiperSlide className="swiper-slide w-[290px] 1072:w-[320px] swiper-fixed-width-300">
             <div className="card-container hover:shadow-[0_12px_16px_hsla(228,66%,45%,.1)] p-3 rounded-2xl w-[290px] 1072:w-[320px] dark:bg-[hsl(228,16%,12%)]">
               <div className="image-wrapper mb-4 relative w-full overflow-hidden object-cover h-[320px]">
                   <Image layout="fill" className="max-w-full h-auto object-cover rounded-2xl" src={home.pictures[0]} />
